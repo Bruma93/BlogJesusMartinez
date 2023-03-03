7d10cff075614d3aa36dfc02575287c86211a12f
Bruma <jmbcompra@gmail.com>
2023-03-03 19:25:40 +0100

    boton email

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,6 +48,7 @@
                         <!-- Authentication Links -->
                         @guest
                             @if (Route::has('login'))
+                            
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                                 </li>
@@ -59,6 +60,9 @@
                                 </li>
                             @endif
                         @else
+                            <li class="nav-item">
+                                    <a class="nav-link " href="{{ url('/email') }}">Enviar email</a>
+                                </li>
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
